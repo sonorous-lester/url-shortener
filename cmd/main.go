71aa1b09088035e635dd9f7e8bee88cbad54de82
Lester <sonorous.lester@gmail.com>
2022-04-05 17:26:19 +0800

    create interface instances and run server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"url-shortener/internal/delivery"
+	"url-shortener/internal/service/config"
+	"url-shortener/internal/service/db"
+	"url-shortener/internal/service/urld"
+	"url-shortener/internal/usecase"
+)
 
+func main() {
+	cfg := config.Read()
+
+	idGenerator := urld.NewGenerator()
+	storage := db.NewSql(cfg)
+	cache := db.NewNosql(cfg)
+	shortenUrlUsecaseImp := usecase.NewShortenUrlUsecase(idGenerator, storage, cache)
+	redirectUsecaseImp := usecase.NewRedirectUsecase(cache)
+
+	defer storage.Close()
+	defer cache.Close()
+
+	router := gin.Default()
+	delivery.NewDeliverHandler(router, cfg.Server.Host, shortenUrlUsecaseImp, redirectUsecaseImp)
+
+	router.Run(cfg.Server.Port)
 }
