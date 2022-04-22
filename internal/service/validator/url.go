f5bc90a3025f3930d0ac5c8a91976df67f89dade
Lester <sonorous.lester@gmail.com>
2022-04-22 18:04:09 +0800

    when new validator failure return error

--- internal/service/validator/url.go
+++ internal/service/validator/url.go
@@ -8,12 +8,12 @@ type UrlValidator struct {
 	reg *regexp.Regexp
 }
 
-func NewUrlValidator() UrlValidator {
+func NewUrlValidator() (UrlValidator, error) {
 	reg, err := regexp.Compile("^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&'\\(\\)\\*\\+,;=.]+$")
 	if err != nil {
-		panic(err)
+		return UrlValidator{}, err
 	}
-	return UrlValidator{reg: reg}
+	return UrlValidator{reg: reg}, nil
 }
 
 func (v UrlValidator) Valid(unchecked string) bool {
