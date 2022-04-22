dcadf4ceb3ae37c4bb959926c5ea1e8175712f93
Lester <sonorous.lester@gmail.com>
2022-04-22 14:30:55 +0800

    add new logger interface

---
+++ internal/domain/log.go
@@ -0,0 +1,12 @@
+package domain
+
+type Logger interface {
+	Errorf(format string, args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Infof(format string, args ...interface{})
+	Info(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Debugf(format string, args ...interface{})
+	Debug(args ...interface{})
+}
