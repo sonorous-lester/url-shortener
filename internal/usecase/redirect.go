14f4538532e8056c067c7b0a46bc41f5fc6e77ab
Lester <sonorous.lester@gmail.com>
2022-04-22 17:38:25 +0800

    add log in redirect usecase

--- internal/usecase/redirect.go
+++ internal/usecase/redirect.go
@@ -1,23 +1,23 @@
 package usecase
 
 import (
-	"fmt"
 	"url-shortener/internal/domain"
 	"url-shortener/internal/exception"
 )
 
 type RedirectUsecase struct {
-	cache domain.Cache
+	cache  domain.Cache
+	logger domain.Logger
 }
 
-func NewRedirectUsecase(cache domain.Cache) *RedirectUsecase {
-	return &RedirectUsecase{cache: cache}
+func NewRedirectUsecase(cache domain.Cache, logger domain.Logger) *RedirectUsecase {
+	return &RedirectUsecase{cache: cache, logger: logger}
 }
 
 func (s *RedirectUsecase) Process(key string) (string, error) {
 	url, err := s.cache.Get(key)
 	if err != nil {
-		fmt.Println(err)
+		s.logger.Errorf("get url from cache error. message: %s", err.Error())
 		return "", exception.ServerError
 	}
 	return url, nil
