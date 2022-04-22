4bdde37972c8d46aeba9bedbbe79fd62cab057fa
Lester <sonorous.lester@gmail.com>
2022-04-22 17:30:11 +0800

    add log in shorten url usecase

--- internal/usecase/shorten_url.go
+++ internal/usecase/shorten_url.go
@@ -11,29 +11,30 @@ type ShortenUrlUsecase struct {
 	generator domain.UrlIdGenerator
 	storage   domain.Storage
 	cache     domain.Cache
+	logger    domain.Logger
 }
 
-func NewShortenUrlUsecase(generator domain.UrlIdGenerator, storage domain.Storage, cache domain.Cache) *ShortenUrlUsecase {
-	return &ShortenUrlUsecase{generator: generator, storage: storage, cache: cache}
+func NewShortenUrlUsecase(generator domain.UrlIdGenerator, storage domain.Storage, cache domain.Cache, logger domain.Logger) *ShortenUrlUsecase {
+	return &ShortenUrlUsecase{generator: generator, storage: storage, cache: cache, logger: logger}
 }
 
 func (s *ShortenUrlUsecase) Process(shortDomainUrl, longUrl, expireAt string) (string, string, error) {
 	urlId := s.generator.NewId()
 	err := s.storage.Store(longUrl, urlId, expireAt)
 	if err != nil {
-		fmt.Println(err)
+		s.logger.Debugf("store url error. longUrl: %s, urlId: %s, expireAt: %s", longUrl, urlId, expireAt)
 		return "", "", exception.ServerError
 	}
 
 	expireTime, err := utils.ToExpireSeconds(expireAt)
 	if err != nil {
-		fmt.Println(err)
+		s.logger.Debugf("expireTime format is invalid. expireTime: %s", expireAt)
 		return "", "", exception.ExpireTimeIsNotValid
 	}
 
 	err = s.cache.Set(urlId, longUrl, expireTime)
 	if err != nil {
-		fmt.Println(err)
+		s.logger.Debugf("url url error. longUrl: %s, urlId: %s, expireAt: %s", longUrl, urlId, expireAt)
 		return "", "", exception.ServerError
 	}
 
